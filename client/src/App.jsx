be2039ca876c4fd6f965eb66640d0876414bd895
Babatunde Alfred Olorundajo <tundealfred.office@gmail.com>
2023-11-02 23:57:53 +0000

    update code init

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,104 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [from, setFrom] = useState("ar");
+  const [to, setTo] = useState("ar");
+  const [word, setWord] = useState("");
+  const [translation, setTranslation] = useState("");
+
+  // onsubmit function that calls our API to get the translation
+  async function handleTranslate(event) {
+    event.preventDefault();
+    const API = `http://localhost:8080/translate?word=${word}&from=${from}&to=${to}`;
+    const res = await axios.get(API);
+    setTranslation(res.data.translation);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={handleTranslate}>
+        <div className="container">
+          <select onChange={(event) => setFrom(event.target.value)}>
+            <option value="ar">Arabic</option>
+            <option value="en">English</option>
+            <option value="pl">Polish</option>
+            <option value="es">Spanish</option>
+            <option value="tr">Turkish</option>
+          </select>
+          <input
+            placeholder="Translate"
+            onChange={(event) => setWord(event.target.value)}
+          />
+        </div>
+
+        <div className="container">
+          <select onChange={(event) => setTo(event.target.value)}>
+            <option value="ar">Arabic</option>
+            <option value="en">English</option>
+            <option value="pl">Polish</option>
+            <option value="es">Spanish</option>
+            <option value="tr">Turkish</option>
+          </select>
+          <div className="output">{translation}</div>
+        </div>
+        <button>Submit</button>
+      </form>
+      {/* show our translation */}
+      {/* STRETCH: show a gif from the GIPHY API that matches the translation */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+// import { useState } from "react";
+// import "./App.css";
+// import axios from "axios";
+
+// function App() {
+//   const [from, setFrom] = useState("ar");
+//   const [to, setTo] = useState("ar");
+//   const [word, setWord] = useState("");
+//   const [translation, setTranslation] = useState({});
+
+//   async function handleTranslate(event) {
+//     event.preventDefault();
+//     const API = `http://localhost:8080/translate?word=hello&from=en&to=es`;
+//     const res = await axios.get(API);
+//   }
+
+//   return (
+//     <>
+//       <form onSubmit={handleTranslate}>
+//         <div className="container">
+//           <select onChange={(event) => setTo(event.target.value)}>
+//             <option value="ar">Arabic</option>
+//             <option value="en">English</option>
+//             <option value="pl">Polish</option>
+//             <option value="es">Spanish</option>
+//             <option value="tr">Turkish</option>
+//           </select>
+//           <input
+//             placeholder="Translate"
+//             onChange={(event) => setWord(event.target.value)}
+//           />
+//         </div>
+
+//         <div className="container">
+//           <select onChange={(event) => setFrom(event.target.value)}>
+//             <option value="ar">Arabic</option>
+//             <option value="en">English</option>
+//             <option value="pl">Polish</option>
+//             <option value="es">Spanish</option>
+//             <option value="tr">Turkish</option>
+//           </select>
+//           <input placeholder="Translate" />
+//         </div>
+//       </form>
+//     </>
+//   );
+// }
+
+// export default App;
--- server/server.js
+++ server/server.js
@@ -8,4 +8,20 @@ const axios = require("axios");
 
 app.get("/", (_, response) => response.json("Root route for translatim."));
 
+app.get("/translate", async (request, response) => {
+  // destructure all the properties from request.query into variables
+  const { word, from, to } = request.query;
+
+  // make our API call
+  const API = `https://api.mymemory.translated.net/get?q=${word}&langpair=${from}|${to}`;
+  const res = await axios.get(API);
+
+  const wrangledData = {
+    translation: res.data.responseData.translatedText,
+    match: res.data.responseData.match,
+  };
+
+  response.json(wrangledData);
+});
+
 app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
