2e21ed624196bff696b2298ef942b72e97beefaa
Babatunde Alfred Olorundajo <tundealfred.office@gmail.com>
2023-11-02 09:59:01 +0000

    git init bruv

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const PORT = process.env.PORT || 8080;
+const app = express();
+app.use(cors());
+const axios = require("axios");
+
+app.get(PORT, (_, response) => response.json("Root route for translatim."));
+
+app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
